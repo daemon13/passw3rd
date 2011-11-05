a2198a7e64f0c60239bf013b83837f5333183e80
Neil Matatall <nmatatall@realpractice.com>
2011-11-04 22:30:48 -1000

    From key rotation, arose cipher changing
    [passw3rd (gh-17-change_cipher *$)]$ rake change_cipher[~/passwords,~/passwords,aes-128-cbc,aes-256-cbc]
    
    [passw3rd (gh-17-change_cipher *$)]$ rake change_cipher[~/passwords,~/passwords,aes-128-cbc,aes-256-cbc]
    [passw3rd (gh-17-change_cipher *$)]$ rake change_cipher[~/passwords,~/passwords,aes-256-cbc,aes-256-cfb]

--- lib/passw3rd/password_service.rb
+++ lib/passw3rd/password_service.rb
@@ -60,6 +60,36 @@ module Passw3rd
         raise err
       end
     end
+    
+    def self.rotate_keys(args = {})
+      unless args.empty?
+        ::Passw3rd::PasswordService.configure do |c|
+          c.password_file_dir = args[:password_file_dir]
+          c.key_file_dir = args[:key_file_dir]
+          c.cipher_name = args[:cipher]
+        end
+      end
+
+      passwords = []
+
+      Dir.foreach(::Passw3rd::PasswordService.password_file_dir) do |passw3rd_file|
+        next if %w{. ..}.include?(passw3rd_file) || passw3rd_file =~ /\A\.passw3rd/
+        puts "Rotating #{passw3rd_file}"
+        passwords << {:clear_password => ::Passw3rd::PasswordService.get_password(passw3rd_file), :file => passw3rd_file}
+      end
+
+      ::Passw3rd::PasswordService.cipher_name = args[:new_cipher] if args[:new_cipher]
+
+      path = ::Passw3rd::KeyLoader.create_key_iv_file
+      puts "Wrote new keys to #{path}"
+
+      passwords.each do |password|
+        full_path = File.join(::Passw3rd::PasswordService.password_file_dir, password[:file])
+        FileUtils::rm(full_path)
+        ::Passw3rd::PasswordService.write_password_file(password[:clear_password], password[:file])    
+        puts "Wrote new password to #{full_path}"
+      end
+    end
 
     protected
 
--- lib/passw3rd/version.rb
+++ lib/passw3rd/version.rb
@@ -1,3 +1,3 @@
 module Passw3rd
-  Version = VERSION = '0.1.2'
+  Version = VERSION = '0.1.3'
 end
